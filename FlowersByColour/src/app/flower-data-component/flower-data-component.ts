c989e6059bd646f0d0ab154e8efa1575c1cf6d26
mphunt2015 <mphunt@us.ibm.com>
2025-08-18 10:16:08 -0500

    Added Flower Data Component

---
+++ FlowersByColour/src/app/flower-data-component/flower-data-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-flower-data-component',
+  imports: [],
+  templateUrl: './flower-data-component.html',
+  styleUrl: './flower-data-component.scss'
+})
+export class FlowerDataComponent {
+
+}
